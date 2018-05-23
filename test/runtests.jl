cb7939cc741958f79b01b030d29ff52f8820c0c2
Galen Lynch <galen.lynch@gmail.com>
2018-05-23 18:47:21 -0400

    Add a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using TSConditioning
 using Base.Test
 
+const A = rand(200)
+
 # write your own tests here
-@test 1 == 2
+@testset "TSConditioning" begin
+    @testset "util" begin
+        @test isapprox(center(A), center(A + 5))
+        @test all(A[end:-1:1] .== rev_view(A))
+        @test rescale([0, 1]) == [0, 1]
+        @test rescale([1, 2]) == [0, 1]
+        @test rescale([0, 1], 2) == [0, 2]
+        @test rescale([1, 2], 2, 1) == [1, 3]
+        (arr, path) = TSConditioning.typemmap(Vector{Int}, (2,); cleanup=false)
+        try
+            @test typeof(arr) == Vector{Int}
+        finally
+            rm(path)
+        end
+    end
+
+    @teset "filt" begin
+        make_hpf_taps(800, 2000)
+        hpf(A, 2000)
+    end
+end
