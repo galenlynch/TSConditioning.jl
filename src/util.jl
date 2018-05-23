a636653ed14b0dc0f6b4287ecc39e54a911b305f
Galen Lynch <galen.lynch@gmail.com>
2018-05-23 18:47:12 -0400

    cleanup files at exit

--- src/util.jl
+++ src/util.jl
@@ -2,8 +2,12 @@ center(x::AbstractArray) = x .- mean(x)
 
 rev_view(a::AbstractVector) = @view a[end:-1:1]
 
-function typemmap(::Type{A}, dims::NTuple{N, Int}, basedir::AbstractString = tempdir()) where {A<:AbstractArray, N}
+function typemmap(
+    ::Type{A}, dims::NTuple{N, Int}, basedir::AbstractString = tempdir();
+    cleanup::Bool = true
+) where {A<:AbstractArray, N}
     (path, io) = mktemp(basedir)
+    cleanup && atexit(()->rm(path))
     arr = try
         Mmap.mmap(io, A, dims; grow = true)
     finally
@@ -11,7 +15,7 @@ function typemmap(::Type{A}, dims::NTuple{N, Int}, basedir::AbstractString = tem
     end
     return (arr, path)
 end
-typemmap(a::A) where A<:AbstractArray = typemmap(A, size(a))
+typemmap(a::A; kwargs...) where A<:AbstractArray = typemmap(A, size(a); kwargs...)
 
 function rescale(x, c = 1, o = 0)
     (s, b) = extrema(x)
