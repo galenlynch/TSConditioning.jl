4ef05088d1e746d322e1101a7e0010397898fc87
Galen Lynch <galen.lynch@gmail.com>
2018-07-22 23:26:47 -0400

    fid-specific naming of files with whiten_mmap

--- src/whitening.jl
+++ src/whitening.jl
@@ -9,8 +9,12 @@ end
 function whiten_mmap(
     xs::AbstractVector{<:AbstractVector{T}},
     w::AbstractArray{S, 2},
-    basedir::AbstractString = tempdir()
+    basedir::AbstractString = tempdir(),
+    fids::AbstractVector{<:Integer} = Int[],
+    autoclean::Bool = true,
+    suffix::AbstractString = "_whitened"
 ) where {T, S}
+    # Input validation
     nx = length(xs)
     nx > 0 || throw(ArgumentError("xs is empty"))
     nel = length(xs[1])
@@ -19,15 +23,35 @@ function whiten_mmap(
     w_sz == (nx, nx) || throw(
         ArgumentError("Size of covariance matrix does not match")
     )
+    if ! isempty(fids) && length(fids) != nx
+        throw(ArgumentError("fids must be empty or same size as xs"))
+    end
+
+    # Pre-allocation of ouputs
     R = promote_type(T, S)
     outs = Vector{Vector{R}}(nx)
     paths = Vector{String}(nx)
+    if isempty(fids)
+        @. paths = joinpath(
+            basedir,
+            string(
+                basename(tempname()), suffix, '_', R, ".dat"
+            )
+        )
+    else
+        @. paths = joinpath(basedir, string(fids, suffix, '_', R, ".dat"))
+    end
+
+    # Make array to be mutated
     for i = 1:nx
         outs[i], paths[i] = typemmap(
-            Vector{R}, (nel,); basedir = basedir, suffix = "_whitened"
+            Vector{R}, (nel,); fpath = paths[i], autoclean = autoclean
         )
     end
+
+    # Whiten
     _whiten_mmap!(outs, xs, w, nx, nel)
+
     outs, paths
 end
 
